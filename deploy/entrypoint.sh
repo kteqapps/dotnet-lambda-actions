0cea5f5c4bb1b71ab0ec96b4986a902bf8d8ec2c
Forevka <32968153+Forevka@users.noreply.github.com>
2020-10-29 16:56:52 +0200

    removed s3 uploading

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,7 +4,7 @@
 export PATH="$PATH:/root/.dotnet/tools"
 cd "${DOTNET_LAMBDA_WORKING_DIR:-.}"
 dotnet lambda package $DOTNET_LAMBDA_PACKAGE_NAME
-aws s3 cp --only-show-errors $DOTNET_LAMBDA_PACKAGE_NAME s3://$DOTNET_LAMBDA_S3_LOCATION/$DOTNET_LAMBDA_PACKAGE_NAME
+
 aws lambda update-function-code \
   --region $AWS_REGION \
   --function-name $DOTNET_LAMBDA_FUNCTION_NAME \
